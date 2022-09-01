ed09d515ed66589e39559caf2a6aa8ab2f4c3f58
Eduardo Perottoni <edu.perottoni@gmail.com>
2022-08-31 23:24:58 -0300

    Aproximação exponencial em C

---
+++ c/aprox_exp.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    //entrada
+    int x = 2;
+    //erro admitido
+    double erro_admitido = 0.00001;
+    //valor exato
+    double exato = exp(x);
+    //variáveis
+    double soma = 1;
+    int potencia = 1, fatorial = 1, contador = 1;
+    double erro = fabs(exato - soma);
+
+    
+
+    while (erro > erro_admitido){
+        potencia = potencia * x;
+        fatorial = fatorial * contador;
+        soma = soma + ((double)potencia / fatorial);
+        erro = fabs(exato - soma);
+        contador++;
+        // printf("%d\n", contador);
+    }
+
+    printf("Soma: %.16lf\n", soma);
+    printf("Nº de termos: %d", contador);
+
+    return 0;
+}
