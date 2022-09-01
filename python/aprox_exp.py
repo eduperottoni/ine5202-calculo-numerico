ae5d3117576e4cbb94dbe58b4b072498d9417b7e
Eduardo Perottoni <edu.perottoni@gmail.com>
2022-08-31 21:34:50 -0300

    Aproximação para exponencial em python

---
+++ python/aprox_exp.py
@@ -0,0 +1,23 @@
+from math import exp
+
+#entrada
+x = 2
+erro_admitido = 0.000001 #1*10^(-6)
+#valor exato
+exato = exp(x)
+#variáveis
+soma = 1
+potencia = 1 #numerador
+fatorial = 1 #denominador
+contador = 1
+erro = abs(exato - soma)
+
+while erro > erro_admitido:
+    potencia *= x
+    fatorial *= contador
+    soma += (potencia/fatorial)
+    erro = abs(exato - soma)
+    contador += 1
+
+print(f'Soma: {soma}')
+print(f'Nº de termos: {contador}')
