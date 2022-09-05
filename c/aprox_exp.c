268ddef49e3dd39082030f6af07ea13b05055149
Eduardo Perottoni <edu.perottoni@gmail.com>
2022-09-05 20:39:15 -0300

    Ajustando aproximação em C

--- c/aprox_exp.c
+++ c/aprox_exp.c
@@ -4,29 +4,27 @@
 
 int main(){
     //entrada
-    int x = 2;
+    int x = 2, contador = 1;
     //erro admitido
-    double erro_admitido = 0.00001;
+    double erro_admitido = 0.000001;
     //valor exato
     double exato = exp(x);
     //variáveis
     double soma = 1;
-    int potencia = 1, fatorial = 1, contador = 1;
+    double potencia = 1, fatorial = 1;
     double erro = fabs(exato - soma);
-
     
-
     while (erro > erro_admitido){
         potencia = potencia * x;
         fatorial = fatorial * contador;
         soma = soma + ((double)potencia / fatorial);
         erro = fabs(exato - soma);
+        //printf("erro na %d iteracao: %f\n", contador, erro);
         contador++;
-        // printf("%d\n", contador);
     }
 
     printf("Soma: %.16lf\n", soma);
-    printf("Nº de termos: %d", contador);
+    printf("N de termos: %d", contador);
 
     return 0;
 }
