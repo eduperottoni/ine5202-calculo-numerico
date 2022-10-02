550de6065203a7ae04c986189661861952e5dd38
Eduardo Perottoni <edu.perottoni@gmail.com>
2022-10-02 16:30:01 -0300

    Refatorando bissecao

--- 02_equacoes-alg-trans/python/bissecao.py
+++ 02_equacoes-alg-trans/python/bissecao.py
@@ -4,32 +4,33 @@ def f(arg):
     #return math.exp(arg) * math.sin(arg) - 1
     return math.e**arg - 2 * math.cos(arg)
 #intervalo
-a = 0
-b = 2
-erro = 10**-8
-conta_ciclos = 0
-fx = 1
+a = 0; b = 2
+erro = 10**-8; conta_ciclos = 0
+fxm = 1; fa = f(a); fb = f(b);
 xm = 2
 
 if (f(a) * f(b) > 0):
     print("O intervalo dado não garante convergência :(")
 else:
-    while (abs(fx) > erro):
+    while (abs(fxm) > erro):
         conta_ciclos += 1
         xm = (a + b) / 2
-        fx = f(xm)
+        fxm = f(xm)
 
-        if (fx * f(a) < 0):
+        if (fxm * f(a) < 0):
             b = xm
-        elif (fx * f(a) > 0):
+            fb = fxm
+        elif (fxm * f(a) > 0):
             a = xm
+            fa = fxm
 
-        print(f'{fx}')
+        print(f'xm = {xm:.16f}')
+        print(f'f(xm) = {fxm:.16f}')
         #input()
 
 print(f'Contador de ciclos: {conta_ciclos}')
-print('Raiz: %.16f' %xm)
-print('Valor f(raiz): %.16f' %fx)
+print('xm (raiz): %.16f' %xm)
+print('f(xm): %.16f' %fxm)
 
 
 
