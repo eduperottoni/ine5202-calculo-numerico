a6592acca59b957d69d53cf5328fda2e76084084
Eduardo Perottoni <edu.perottoni@gmail.com>
2022-12-04 23:48:20 -0300

    Implementação trapezios

--- 07_integracao/trapezios.m
+++ 07_integracao/trapezios.m
@@ -1,18 +1,25 @@
 clc
 clear
+
+% limites de integração
 a = 0;
 b = 1;
 
-N = 10;
-h = (b - a) / N;
+% número de subintervalos
+n = 10;
+
+% definindo altura de cada trapézio
+h = (b - a) / n;
 x = [a:h:b];
 
-% função interna à integral
+% função a ser integrada
 y = exp(x);
 
 s = 0;
-for i = 2 : N
+for i = 2 : n
   s += y(i);
 endfor
 
-T = (h/2)*(y(1) + 2*s + y(N+1))
+T = (h/2)*(y(1) + 2*s + y(n+1));
+
+printf("Integral pela regra do trapézio = %.16f", T);
