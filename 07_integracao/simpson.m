e3fb55742698efd9d3128efdf930d07bfd5eaf24
Eduardo Perottoni <edu.perottoni@gmail.com>
2022-12-07 16:47:12 -0300

    Implementação de quadratura gaussiana

--- 07_integracao/simpson.m
+++ 07_integracao/simpson.m
@@ -20,6 +20,15 @@ for i = 1 : (n_par / 2 - 1)
    soma_impares += y(2 * i + 1);
 endfor
 
+% ou, com uma estrutura de repetição apenas:
+%for i = 1 : n
+%  if i % 2 == 0:
+%    soma_pares += y(i)
+%  else
+%    soma_impares += y(i)
+%  endif
+%endfor
+
 S = (h/3) * (y(1) + 4 * soma_pares + 2 * soma_impares + y(n_par + 1));
 
 printf("| Aproximação da integral por Simpson = %.16f", S);
